60a835c6ce9378f9d510881d79af0f55f8c07035
Trevor Hartman <trevorhartman@gmail.com>
2015-11-03 16:39:43 -0700

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot-stackstorm "0.1.4"
+(defproject yetibot-stackstorm "0.1.5-SNAPSHOT"
   :description "yetibot Stackstorm plugin"
   :url "https://github.com/devth/yetibot.core"
   :scm {:name "git" :url "https://github.com/devth/yetibot-stackstorm.git"}
